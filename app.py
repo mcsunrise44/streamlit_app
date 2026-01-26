ae308534d5310de4369796add8eab83e8c4494ae
mcsunrise44 <T125075@ed.sus.ac.jp>
2026-01-26 11:24:15 +0900

    8st commit

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ df = pd.read_csv('FEH_00500215_260126105748.csv')
 
 with st.sidebar:
     st.subheader('抽出条件')
-    a = st.multiselect('野菜の種類を選択してください',
+    vesitable_series = st.multiselect('野菜の種類を選択してください',
                        df['(F005-05-2-002)品目'])
     st.subheader('色分け')    
     color = st.selectbox('分類を選択してください',
-                      ['作付面積【ha】', '収穫量【t】', '出荷量【t】'])  
+                      ['作付面積【ha】', '収穫量【t】', '出荷量【t】'])
+    
+df = df[df['(F005-05-2-002)品目'].isin(vesitable_series)]
+
+st.dataframe(df)
