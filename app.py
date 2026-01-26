65dbbaa38d75dc1dd110707d2d9bc451ca85d8c5
mcsunrise44 <T125075@ed.sus.ac.jp>
2026-01-26 11:28:54 +0900

    9st commit

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 st.title('野菜収穫量/出荷量')
 
-df = pd.read_csv('FEH_00500215_260126105748.csv')
+df = pd.read_csv(
+    'FEH_00500215_260126105748.csv',
+    sep='\t',
+    encoding='utf-8'
+)
 
 with st.sidebar:
     st.subheader('抽出条件')
