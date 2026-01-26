25b8c37e484d259a0ae6c1a35a987892ff7ad495
mcsunrise44 <T125075@ed.sus.ac.jp>
2026-01-26 11:08:17 +0900

    6st commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-st.title('全国人口推移')
+st.title('野菜収穫量/出荷量')
 
-df = pd.read_csv('05k2-2.csv')
+df = pd.read_csv('FEH_00500215_260126105748.csv')
+
+with st.sidebar:
+    st.subheader('抽出条件')
+    a = st.multiselect('野菜の種類を選択してください',
+                       df['(F005-05-2-002)品目'])
