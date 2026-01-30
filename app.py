ed8c9cb27406e8f9bd5cf7796d5310504b3183cd
mcsunrise44 <T125075@ed.sus.ac.jp>
2026-01-30 15:25:42 +0900

    15st commit

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ df = pd.read_csv(
 with st.sidebar:
     st.subheader('抽出条件')
     vesitable_series = st.multiselect('条件を選択してください（複数選択可）',
-                       df['全国・都道府県 コード'].unique())
-    st.subheader('色分け')
-    color = st.selectbox('分類を選択してください',
-                      ['男女別 コード','人口 コード','2005年','2010年','2015年','2020年','2021年','2022年','2023年','2024年'])
-    
-df = df[df['全国・都道府県 コード'].isin(vesitable_series)]
+                       df['全国・都道府県'].unique())
+
+df = df[df['全国・都道府県'].isin(vesitable_series)]
+
+df.drop('表章項目 コード',axis=1,inplace=True)
+df.drop('表章項目 補助コード',axis=1,inplace=True)
+df.drop('表章項目',axis=1,inplace=True)
+df.drop('男女別 コード',axis=1,inplace=True)
+df.drop('男女別 補助コード',axis=1,inplace=True)
+df.drop('人口 コード',axis=1,inplace=True)
+df.drop('人口 補助コード',axis=1,inplace=True)
+df.drop('全国・都道府県 コード',axis=1,inplace=True)
+df.drop('全国・都道府県 補助コード',axis=1,inplace=True)
+df.drop('/時間軸（年）',axis=1,inplace=True)
 
 df.columns = df.columns.str.replace('\ufeff', '', regex=False).str.strip()
 st.dataframe(df)
